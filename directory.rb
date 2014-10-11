623345f9f721a84202a815f6c2f494c373a29c2d
danjocutler <danjocutler@gmail.com>
2014-10-11 16:58:32 +0100

    Added save option and .gitignore

--- directory.rb
+++ directory.rb
@@ -18,22 +18,19 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
+  while !name.empty? do   
     @students << {:name => name, :cohort => :november}    
     puts "Now we have #{@students.length} students"
-    # get another name from the user
     name = gets.chomp
-  end
+  end  
 end
 
 def print_menu    
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items  
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" 
 end
 
 def show_students
@@ -42,12 +39,24 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+    save_students    
   when "9"
     exit
   else
