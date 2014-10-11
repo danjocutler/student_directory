4cacdd4c6e53e8ada357c0f0a7daf00f45a02a49
danjocutler <danjocutler@gmail.com>
2014-10-11 17:09:12 +0100

    load option added

--- directory.rb
+++ directory.rb
@@ -18,10 +18,14 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # get the first name
   name = gets.chomp
-  while !name.empty? do   
-    @students << {:name => name, :cohort => :november}    
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    add_student(name, :november)
     puts "Now we have #{@students.length} students"
+    # get another name from the user
     name = gets.chomp
   end  
 end
@@ -30,7 +34,8 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
-  puts "9. Exit" 
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
 def show_students
@@ -40,11 +45,26 @@ def show_students
 end
 
 def save_students
+  # open the file for writing
   file = File.open("students.csv", "w")
+  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def add_student(name, cohort)
+  @students << {:name => name, :cohort => cohort.to_sym}
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    add_student(name, cohort)
   end
   file.close
 end
@@ -57,6 +77,8 @@ def process(selection)
     show_students
   when "3"
     save_students    
+  when "4"
+    load_students
   when "9"
     exit
   else
