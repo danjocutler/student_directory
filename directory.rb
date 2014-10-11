13913d56c7b419e289d22c356fc526719f4b4b78
danjocutler <danjocutler@gmail.com>
2014-10-11 16:40:09 +0100

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,18 @@
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)    
+  puts "Overall, we have #{names.length} great students"
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,7 +23,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :September}    
+    students << {:name => name, :cohort => :november}    
     puts "Now we have #{students.length} students"
     # get another name from the user
     name = gets.chomp
@@ -17,23 +32,29 @@ def input_students
   students
 end
 
-#first we print the list of students
-def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-------------"
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
 end
 
-def print(students)
-	students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
-end
-
-#finally, we print the total
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
-end
-
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
