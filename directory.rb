e2d6f9edc35778127daa0242d6729e0e4f8f5da7
danjocutler <danjocutler@gmail.com>
2014-10-11 15:52:28 +0100

    Added methods for better readability

--- directory.rb
+++ directory.rb
@@ -5,8 +5,21 @@ students = %w(Dan Hadi Craig Andrew Tim Steve Andrew
 			Camilla Ella Ana Anna Rachel Elena Karin)
 
 #first we print the list of students
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each {|student| puts student}
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-------------"
+end
+
+def print(names)
+	names.each {|name| puts name}
+end
+
 #finally, we print the total
-print "Overall, we have #{students.length} great students"
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
